5a014cf910f061e84c39d343af8bb9e6a2693360
aphinith <aphinith84@gmail.com>
2016-03-14 15:16:41 -0700

    first commit

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className) {
   // your code here
+  //iterate through the body to get access to nodes
+  //if nodes match, push that match to results;
+  //logging to see what document.body is
+
+  //create variable to hold results
+  var matches = [];
+  //create function to run search
+  var findMatches = function(node) {
+    //loop through node
+    _.each(node, function(val) {
+      if (className === val) {
+        console.log('this is test:', node)
+      }
+    })
+    
+  }
+  console.log("this is matches:", matches)
+
+  findMatches(document.body)
+  return matches;
+
+
+  // console.log("this is body:", document.body);
+  // //logging to see what element.childNodes is
+  // console.log('this is element.childNodes:', document.body.childNodes);
+  // //logging to see what element.classList is
+  // console.log('this is element.classList:', document.body.classList);
+  // //testing log to element.classList
+  // console.log('this is element.classList at index 1:', document.body.classList[1]);
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,40 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj === 'number' || typeof obj === 'boolean' || obj === null){
+    return obj + "";
+  //check to see if object is a string
+  } else if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  //check if obj is ar"{" + "}";
+  if(Array.isArray(obj)){
+    //check if obj.length is 0
+    //create array to collect results
+    var tempArr = [];
+    if(obj.length === 0){
+      return "[]";
+    } else {
+      _.each(obj, function(item, index, list){
+        tempArr.push(stringifyJSON(item));
+      })
+    }
+    return "[" + tempArr + "]";
+  }   
+  //check to see if obj is object
+  else {
+    //loop thru obj using for loop
+    for(var key in obj){
+      //check if property value is undefined or a function
+      if(obj[key] === undefined || typeof obj[key] === 'function')
+        return "{}";
+    }
+    //create empty array to collect results
+    var emptyArr = [];
+    //iterate thru obj and push results to emptyArr
+    _.each(obj, function(item, key, list){
+      emptyArr.push(stringifyJSON(key) + ':' + stringifyJSON(item));
+    })
+    return '{' + emptyArr + '}';
+  }
 };
